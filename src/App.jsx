ea3291357ab4bc2c986c89ee63f588c5c0989cd7
Raj upadhyay <rajgit2003@gmail.com>
2024-04-18 00:21:52 +0530

    Todo List with the help of React js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Create from "./components/Create";
+import Show from "./components/Show";
+
+const App = () => {
+    const [tasks, settasks] = useState([]);
+
+    return (
+        <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
+            <Header tasks={tasks} />
+            <Create tasks={tasks} settasks={settasks} />
+            <Show tasks={tasks} settasks={settasks} />
+        </div>
+    );
+};
+
+export default App;
